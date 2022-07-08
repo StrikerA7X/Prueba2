8a244a25067e26f54da43bdb14184b0761dc9ea6
TUF GAMING <TUF GAMING@LAPTOP-E7M3AB9F>
2022-07-08 07:40:05 -0500

    Prueba02

---
+++ src/main/java/Figuras/Principal.java
@@ -0,0 +1,16 @@
+package Figuras;
+
+/**
+ *
+ * @author USER
+ */
+public class Principal {
+
+    public static void main(String[] args){
+        
+        VentanaDibujar v1 = new VentanaDibujar();
+        v1.setVisible(true);
+        
+    }
+
+}
